402188fb7922a810ebaee01e5f043e42daba34b2
aeriations <designs.aeria@gmail.com>
2026-08-16 13:03:56 +0100

    ignoring

--- src/tools/misc.py
+++ src/tools/misc.py
@@ -24,3 +24,26 @@ def open_app(app_name: str) -> str:
                 return f"Launched {filename[:-4]}."
 
     return f"No shortcut found for '{app_name}'."
+
+def go_to_discord_direct_messages(name: str):
+    result = keyboard.focus_window("Discord")
+    if "No window found" in result:
+        return "Discord doesn't seem to be open"
+    time.sleep(0.5)
+    keyboard.press_combo("ctrl", "k")
+    time.sleep(0.4)
+
+    keyboard.type_text(name, 0.03)
+    time.sleep(0.6)
+
+    keyboard.press_key("enter")
+
+    return f"Went to {name}'s direct messages on discord."
+
+def call_on_discord(name: str):
+    result = go_to_discord_direct_messages(name)
+    if "No window found" in result:
+        return "Discord doesn't seem to be open"
+
+    keyboard.press_combo("ctrl", "backtick")
+    return f"Called {name} on discord."
